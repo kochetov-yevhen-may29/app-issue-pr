f946e1bb5fac647baa0aa14f33e0ea02551a0c58
Yevhen <ko4etov.evgeniy@gmail.com>
2023-10-17 22:09:56 +0300

    Connect to the db and create issue model

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,30 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
-import { BsBugFill } from 'react-icons/bs';
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { BsBugFill } from "react-icons/bs";
+import cn from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const links = [
-    { label: 'Dashboard', href: '/'},
-    { label: 'Issues', href: '/issues'},
-  ]
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  console.log(currentPath);
   return (
-    <nav className='flex space-x-6 items-center h-14 mb-5 px-5 border-b'>
+    <nav className="flex space-x-6 items-center h-14 mb-5 px-5 border-b">
       <Link href="/">
         <BsBugFill />
       </Link>
-      <ul className='flex space-x-6'>
+      <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
             key={link.href}
             href={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={cn({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
