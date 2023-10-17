f33f684fb80ded043afb2f698509ca765aa87c00
Yevhen <ko4etov.evgeniy@gmail.com>
2023-10-17 19:42:47 +0300

    Build the Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,13 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
+import { BsBugFill } from 'react-icons/bs';
 
 const Navbar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/'},
+    { label: 'Issues', href: '/issues'},
+  ]
   return (
     <nav className='flex space-x-6 items-center h-14 mb-5 px-5 border-b'>
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <BsBugFill />
+      </Link>
       <ul className='flex space-x-6'>
-        <li><Link href="/"></Link>Dashboard</li>
-        <li><Link href="/issues"></Link>Issues</li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   )
